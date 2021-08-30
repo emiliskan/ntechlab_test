b41b58843d6e67415e7f901615a0869949d95cde
Эмиль Искандеров <emilagamamedov@gmail.com>
2021-08-30 21:33:27 +0300

    #3 added migrations

--- db/database.py
+++ db/database.py
@@ -1,12 +1,8 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
-from core.config import POSTGRES_DB_NAME, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_USER, POSTGRES_HOST
+from core.config import SQLALCHEMY_DATABASE_URL
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@" \
-                          f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
